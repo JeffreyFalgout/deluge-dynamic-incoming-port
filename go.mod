e579606e9111bd53f5a5080fdf84d7d5ee02b05a
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2023-09-03 12:36:38 -0600

    Spruce up main.go.
    Separate things out a little better, add better logging.

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/JeffreyFalgout/deluge-dynamic-incoming-port
 
 go 1.21
 
-require github.com/jackpal/go-nat-pmp v1.0.2
+require (
+	github.com/jackpal/go-nat-pmp v1.0.2
+	github.com/rs/zerolog v1.30.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+)
--- main.go
+++ main.go
@@ -3,69 +3,124 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"time"
 
 	natpmp "github.com/jackpal/go-nat-pmp"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	ctx := context.Background()
 
+	out := zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
+		w.Out = os.Stderr
+		w.TimeFormat = time.RFC3339
+	})
+	log.Logger = zerolog.New(out).With().Timestamp().Logger()
+	zerolog.DefaultContextLogger = &log.Logger
+
 	gateway, ok := os.LookupEnv("NATPMP_GATEWAY")
 	if !ok {
-		log.Fatalf("You must specify NATPMP_GATEWAY as an environment variable.")
+		log.Fatal().
+			Msg("You must specify NATPMP_GATEWAY as an environment variable.")
 	}
 	gatewayIP := net.ParseIP(gateway)
 	if gatewayIP == nil {
-		log.Fatalf("NATPMP_GATEWAY was invalid: %q", gateway)
+		log.Fatal().
+			Str("NATPMP_GATEWAY", gateway).
+			Msg("NATPMP_GATEWAY is invalid")
 	}
 
-	var mapping *natpmp.AddPortMappingResult
-	timeout := 250 * time.Millisecond
+	deluge := newDelugeClient("localhost:8117")
+	minTimeout := 250 * time.Millisecond
+	maxTimeout := 5 * time.Minute
+	timeout := minTimeout
 	for {
-		start := time.Now()
-		err := func() error {
-			log.Printf("Attempting with %v timeout", timeout)
+		func() {
+			log.Debug().
+				Dur("timeout", timeout).
+				Msg("Attempting to map a port...")
 			ctx, cancel := context.WithTimeout(ctx, timeout)
 			defer cancel()
 
-			c := natpmp.NewClientWithTimeout(gatewayIP, timeout)
-			addr, err := c.GetExternalAddress()
+			ttl, err := run(ctx, gatewayIP, deluge)
 			if err != nil {
-				return fmt.Errorf("could not get external address: %w", err)
-			}
-			log.Printf("External address: %v", net.IP(addr.ExternalIPAddress[:]))
-
-			newMapping, err := c.AddPortMapping("tcp", 0, 0, 360)
-			if err != nil {
-				return fmt.Errorf("could not add port mapping: %w", err)
-			}
-			log.Printf("Added port mapping %d -> %d for %v", newMapping.InternalPort, newMapping.MappedExternalPort, time.Duration(newMapping.PortMappingLifetimeInSeconds)*time.Second)
-
-			if mapping == nil || mapping.MappedExternalPort != newMapping.MappedExternalPort {
-				if err := notifyDeluge(ctx, newMapping.MappedExternalPort); err != nil {
-					return fmt.Errorf("could not notify deluge about the port mapping: %w", err)
-				}
-				mapping = newMapping
+				timeout = min(timeout*2, maxTimeout)
+				log.Error().Err(err).Send()
+				dl, _ := ctx.Deadline()
+				time.Sleep(time.Until(dl))
+				return
 			}
 
-			timeout = max(timeout/2, 250*time.Millisecond)
-			time.Sleep(time.Duration(mapping.PortMappingLifetimeInSeconds) * time.Second / 2)
-			return nil
+			timeout = max(timeout/2, minTimeout)
+			time.Sleep(ttl / 2)
 		}()
-		if err != nil {
-			timeout *= 2
-			log.Print(err)
-			dur := time.Since(start)
-			sleep := timeout - dur
-			time.Sleep(sleep)
-		}
 	}
 }
 
-func notifyDeluge(ctx context.Context, port uint16) error {
-	return fmt.Errorf("TODO")
+func run(ctx context.Context, gateway net.IP, d *deluge) (time.Duration, error) {
+	p, ttl, err := requestPortMapping(ctx, gateway)
+	if err != nil {
+		return 0, err
+	}
+	if err := d.updateIncomingPort(ctx, p); err != nil {
+		return 0, err
+	}
+	return ttl, nil
+}
+
+func requestPortMapping(ctx context.Context, gateway net.IP) (uint16, time.Duration, error) {
+	log := log.Ctx(ctx)
+
+	var c *natpmp.Client
+	dl, ok := ctx.Deadline()
+	if ok {
+		c = natpmp.NewClientWithTimeout(gateway, time.Until(dl))
+	} else {
+		c = natpmp.NewClient(gateway)
+	}
+	addr, err := c.GetExternalAddress()
+	if err != nil {
+		return 0, 0, fmt.Errorf("could not get external address: %w", err)
+	}
+	log.Debug().
+		IPAddr("external_address", net.IP(addr.ExternalIPAddress[:])).
+		Send()
+
+	m, err := c.AddPortMapping("tcp", 0, 0, 360)
+	if err != nil {
+		return 0, 0, fmt.Errorf("could not add port mapping: %w", err)
+	}
+
+	ttl := time.Duration(m.PortMappingLifetimeInSeconds) * time.Second
+	log.Debug().
+		Uint16("internal_port", m.InternalPort).
+		Uint16("external_port", m.MappedExternalPort).
+		Dur("ttl", ttl).
+		Msg("Added port mapping")
+	return m.MappedExternalPort, ttl, nil
+}
+
+type deluge struct {
+	addr string
+	port uint16
+}
+
+func newDelugeClient(addr string) *deluge {
+	return &deluge{addr: addr}
+}
+
+func (d *deluge) updateIncomingPort(ctx context.Context, port uint16) error {
+	log := log.Ctx(ctx)
+	if port == d.port {
+		log.Debug().
+			Uint16("port", port).
+			Msg("Not updating Deluge since we think its port mapping is already correct.")
+		return nil
+	}
+
+	return fmt.Errorf("todo")
 }
